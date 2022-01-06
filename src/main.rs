2ed13eaa6aab9e5b45e5004044a765e6c1353d4f
Steve Hayes <steve.j.hayes@gmail.com>
2022-01-06 10:28:04 +0000

    Updated so that it works with break after for loop

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,9 @@ use std::io::{self, Read};
   fn main() {
     for number in io::stdin().bytes() {
         let character:char = number.unwrap() as char;
-        if character == 'q' {
-            break; 
         println!("Hello {}", character);
+        if character == 'q' {
+            break;
      }
     } 
   }
